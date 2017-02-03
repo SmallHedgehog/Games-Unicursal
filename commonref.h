01d720564685059cff0fe66e2aa1a2b984f90777
caifei yang <caifei yang>
2017-02-03 16:05:07 +0800

    Define common variables(eg. WINDOW_WIDTH)

---
+++ commonref.h
@@ -0,0 +1,9 @@
+#ifndef COMMONREF_H
+#define COMMONREF_H
+
+#define WIN_WIDTH       100
+#define WIN_HEIGHT      100
+
+#define MIN_DISTANCE    0.1     // 当对应顶点的连线进行操作时(最大的选择距离)
+
+#endif // COMMONREF_H
